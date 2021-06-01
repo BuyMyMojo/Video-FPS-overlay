ce4ea3a298bd012e04e9253a71f76f9a2d1983d4
Owen Quinlan <46948241+BuyMyMojo@users.noreply.github.com>
2021-06-02 02:22:31 +1000

    Code clean up
    Changed how some of the code worked thanks to HunterAP.
    
    Introduced a weird bug where the plot data range has to be increased by 20.
    
    Added 720p option too

--- AnimPlot1.py
+++ AnimPlot1.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+
 # importing the required modules
-import matplotlib.pyplot as plt
-import csv
+import argparse as argp
+import os
+import statistics
+import sys
+
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import numpy as np
 from pandas import *
 import gc
-
 
 # settings
 # x axis values (30 frame range for testing)
@@ -15,17 +22,7 @@ CSVPath = "FrameView-CSV\FrameView_Cyberpunk2077.exe_2021_05_31T190026_Log.csv"
 OutFolder = "test video\\"
 colour = "r"
 
-if Resolution == 1080:
-    DPI = 120
-elif Resolution == 1440:
-    DPI = 160
-elif Resolution == 2160:
-    DPI = 240
 
-
-Xaxis = []
-for x in range(PresetFrameRange):
-  Xaxis.append(x+1)
 # reading CSV file
 FpsData = read_csv("FrameView-CSV\\FrameView_Cyberpunk2077.exe_2021_05_31T190026_Log.csv")
 
@@ -43,47 +40,40 @@ for j in range(PresetFrameRange):
 for i in range(VideoFrames):
     plt.close("all")
     trimRange = PresetFrameRange+i
+
+    Xaxis = []
+    Xaxis = [t for t in range(PresetFrameRange)]
+    # plt.xlim(Xaxis[0] - Xaxis[60], Xaxis[60])
+
+    # setup graph
+    fig, ax = plt.subplots()
+    fig.patch.set_alpha(0.0)
+    if Resolution == 720:
+        DPI = 45
+    elif Resolution == 1080:
+        DPI = 120
+    elif Resolution == 1440:
+        DPI = 160
+    elif Resolution == 2160:
+        DPI = 240
     
-
-    # add frame times here to be trimmed
-    # TrimFrameTimes = FullFrameTimes[i:trimRange]
-
-
-    TrimFrameRate = FUllFrameRate[i:trimRange]
-
-
-    Yaxis = TrimFrameRate
-
-    plt.close('all')
-    # set plot size and limit
-    plt.figure(figsize=(16, 4), dpi=DPI)
-    plt.ylim([0,120])
-    # plotting the points 
-    lines = plt.plot(Xaxis, Yaxis)
-    
-    
-    # naming the x axis
-    # plt.xlabel('Frame')
-
-    # naming the y axis
-    plt.ylabel('FPS')
-    
-    # giving a title to my graph
-    plt.title(Title)
-
-    # change graph settings
-    plt.setp(lines, c=colour)
+    fig.dpi = DPI
+    fig.set_size_inches(16, 4)
+    ax.set_ylim(0, 120)
+    ax.set_xlim(Xaxis[0], Xaxis[60])
+    lines = ax.plot(Xaxis, FUllFrameRate[i+20:trimRange+20], color="red")
+    # lines.set_data(Xaxis, FUllFrameRate[i:trimRange])
+    ax.set_ylabel('FPS')
+    ax.set_title(Title)
 
     # save as png
     plt.savefig(OutFolder + "Frame_" + str(i+1) + '.png', transparent=False)
-    plt.clf()
-    plt.cla()
+    ax.cla()
     plt.close('all')
     
     print('Processed frame ' + str(i+1) + ' of ' + str(VideoFrames) + ' FPS graph')
+
     del trimRange
-    # del TrimFrameTimes
-    del TrimFrameRate
-    del Yaxis
+    del Xaxis
     del lines
     gc.collect()
