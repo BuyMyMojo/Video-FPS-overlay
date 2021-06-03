2871eb5642ada2c02e4df3c2d55bef23df9fe0c0
Owen Quinlan <46948241+BuyMyMojo@users.noreply.github.com>
2021-06-03 19:45:43 +1000

    Added support for MSI Afterburner .hml file

--- VideoFpsOverlay.py
+++ VideoFpsOverlay.py
@@ -34,6 +34,13 @@ def main(args):
     ymax = args.ymax
     TextColour = args.tc
 
+    if args.f == "NV":
+        FpsGraphFV(CSVPath, transparentBackground, Resolution, Title, PresetFrameRange, colour, BackColour, OutFolder, LineWidth, RemoveBox, RemoveNumbers, ymin, ymax, TextColour)
+    elif args.f == "MS":
+        FpsGraphMS(CSVPath, transparentBackground, Resolution, Title, PresetFrameRange, colour, BackColour, OutFolder, LineWidth, RemoveBox, RemoveNumbers, ymin, ymax, TextColour)
+
+
+def FpsGraphFV(CSVPath, transparentBackground, Resolution, Title, PresetFrameRange, colour, BackColour, OutFolder, LineWidth, RemoveBox, RemoveNumbers, ymin, ymax, TextColour):
 
     # reading CSV file
     FpsData = read_csv(CSVPath)
@@ -48,11 +55,82 @@ def main(args):
     for j in range(PresetFrameRange):
         # FullFrameTimes.insert(0, 0) # to add back in later when implamenting frametime graph
         FUllFrameRate.insert(0, 0)
-    
-    FpsGraphNvidiaFrameview(transparentBackground, Resolution, VideoFrames, Title, PresetFrameRange, FUllFrameRate, colour, BackColour, OutFolder, LineWidth, RemoveBox, RemoveNumbers, ymin, ymax, TextColour)
+
+    # setup graph
+    fig, ax = plt.subplots()
+    Transparency = 1.0
+    if transparentBackground == True:
+        Transparency = 0.0
 
 
-def FpsGraphNvidiaFrameview(transparentBackground, Resolution, VideoFrames, Title, PresetFrameRange, FUllFrameRate, colour, BackColour, OutFolder, LineWidth, RemoveBox, RemoveNumbers, ymin, ymax, TextColour):
+    fig.patch.set_alpha(Transparency)
+
+
+    if Resolution == 720:
+        DPI = 45
+    elif Resolution == 1080:
+        DPI = 120
+    elif Resolution == 1440:
+        DPI = 160
+    elif Resolution == 2160:
+        DPI = 240
+
+
+    # generate graph frames
+    start = time()
+    for i in range(VideoFrames):
+        trimRange = PresetFrameRange+i
+
+        Xaxis = []
+        Xaxis = [t for t in range(PresetFrameRange)]
+
+        lines = ax.plot(Xaxis, FUllFrameRate[i:trimRange], color=colour, linewidth=LineWidth)
+
+        ax.set_ylim(ymin, ymax)
+        fig.dpi = DPI
+        fig.set_size_inches(16, 4)
+        ax.set_ylabel('FPS')
+        ax.set_title(Title, {'color': TextColour})
+        ax.xaxis.label.set_color(TextColour)
+        ax.yaxis.label.set_color(TextColour)
+        ax.tick_params(axis='both', colors=BackColour, bottom=RemoveBox, top=RemoveBox, left=RemoveBox, right=RemoveBox, labelleft=RemoveNumbers, labelbottom=RemoveNumbers)
+        ax.spines['left'].set_visible(RemoveBox)
+        ax.spines['right'].set_visible(RemoveBox)
+        ax.spines['bottom'].set_visible(RemoveBox)
+        ax.spines['top'].set_visible(RemoveBox)
+        ax.spines['left'].set_color(BackColour)
+        ax.spines['right'].set_color(BackColour)
+        ax.spines['top'].set_color(BackColour)
+        ax.spines['bottom'].set_color(BackColour)
+
+        # save as png
+        plt.savefig(OutFolder + "Frame_" + str(i+1) + '.png', transparent=transparentBackground)
+        ax.cla()
+
+        print('Processed frame ' + str(i+1) + ' of ' + str(VideoFrames) + " " + str((i+1)*100/VideoFrames)[0:5] + "%" + ' FPS graph')
+
+        del trimRange
+        del Xaxis
+        del lines
+        gc.collect()
+
+    print("Completed!")
+    print(f'Time taken: {time() - start}')
+
+def FpsGraphMS(CSVPath, transparentBackground, Resolution, Title, PresetFrameRange, colour, BackColour, OutFolder, LineWidth, RemoveBox, RemoveNumbers, ymin, ymax, TextColour):
+
+    # reading CSV file
+    FpsData = read_csv(CSVPath, skiprows=2, usecols=[26], squeeze=True)
+
+    # grab all frame times
+    FUllFrameRate = FpsData.tolist()
+    FUllFrameRate = FUllFrameRate[31:]
+    VideoFrames = len(FUllFrameRate)
+    gc.collect()
+    # add Frame Range -1 blank values at the start for the animation
+    for j in range(PresetFrameRange):
+        # FullFrameTimes.insert(0, 0) # to add back in later when implamenting frametime graph
+        FUllFrameRate.insert(0, 0)
 
     # setup graph
     fig, ax = plt.subplots()
@@ -120,10 +198,11 @@ parser = argp.ArgumentParser(description='Generates image sequences from FPS/Fra
 
 # add arguments
 parser.add_argument('CSV', metavar='CSV', type=str, help='The path to your CSV file')
+parser.add_argument('-f', metavar='Format', type=str, help='Choose what format the csv is in [FV = FrameView, MS = MSI afterburner, MH = MangoHud] [default: FrameView]', default="FV")
 parser.add_argument('Output', metavar='Output', type=str, help='The path your image sequence will be saved')
 parser.add_argument('-r', metavar='Resolution', type=int, help='Resolution of recording [720, 1080, 1440, 2160] [default: 1080]', default=1080)
 parser.add_argument('-t', metavar='Title', type=str, help='Set the title of the graph', default=" ")
-parser.add_argument('-dr', metavar='Range', type=int, help='Sets the X length of the graph in data points (example: 90 will have the graph show 90 data values at once) ', default=90)
+parser.add_argument('-dr', metavar='Range', type=int, help='Sets the X length of the graph in data points (example: 90 will have the graph show 90 data values at once) [default : 120]', default=120)
 parser.add_argument('-lc', metavar='Colour', type=str, help='Set the colour of the line [format: matplotlib colors] [default: red]', default="r")
 parser.add_argument('-bc', metavar='Colour', type=str, help='Set the colour of the axis and markers [format: matplotlib colors] [default: black]', default="black")
 parser.add_argument('-tc', metavar='Colour', type=str, help='Set the colour of the text [format: matplotlib colors] [default: black]', default="black")
